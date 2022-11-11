e74f82cac8fed8718aef397e6a0d4c0462bdcc77
BraianAzcune <braian.s.azcune@hotmail.com>
2022-11-11 00:07:28 -0300

    parametrizar red convolucional, para poder ser ajustada en las pruebas

--- modelo_convolucional.py
+++ modelo_convolucional.py
@@ -1,36 +1,50 @@
 from numpy import pad
 import torch
 from torch import nn
+from math import floor
 
 """
 TODO hay que revisar entre Lenet5 y AlexNet, las diferencias entre ambas, y asi crear un rango de busqueda para los hiperparametros que definen la estructura interna de la red.
 """
 
+
 class RedConvolucional(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, cant_filtros_conv1=6, kernel_size_maxpool1=2, cant_filtros_conv2=16, kernel_size_maxpool2=2,full_l1=120,full_l2=84) -> None:
         super().__init__()
         # se reciben imagenes 3x32x32
         self.layer1 = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5, stride=1, padding=0), # quedan un volumen de 6x28x28 # (n - k + s + p)/s
+            # quedan un volumen de cant_filtros_conv1x30x30 # (n - k + s + p)/s
+            nn.Conv2d(in_channels=3, out_channels=cant_filtros_conv1,
+                      kernel_size=5, stride=1, padding=2),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2,stride=2) # queda un volumen de 6x14x14
+            # queda un volumen de cant_filtros_conv1 x 15 x 15 (si kernel_size_maxpool1=2, si es 3, entonces= 14)
+            nn.MaxPool2d(kernel_size=kernel_size_maxpool1, stride=2)
         )
 
+        o_conv2d = [cant_filtros_conv1, 32-5+1+2] # 30
+        o_maxPool = [cant_filtros_conv1, floor((o_conv2d[1] - kernel_size_maxpool1 + 2)/2)]
+
         self.layer2 = nn.Sequential(
-            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1, padding=0), # queda un volumen de 16x10x10
+            nn.Conv2d(in_channels=o_maxPool[0], out_channels=cant_filtros_conv2,
+                      kernel_size=5, stride=1, padding=0),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2) # queda un volumen de 16x5x5 = 400.
+            nn.MaxPool2d(kernel_size=kernel_size_maxpool2, stride=2)
         )
+
+        o_conv2d2 = [cant_filtros_conv2, o_maxPool[1] - 5 + 1]
+        o_maxPool2 = [cant_filtros_conv2, floor((o_conv2d2[1]-kernel_size_maxpool2+2)/2)]
+
+        cantidad_entradas = floor(o_maxPool2[0] * pow(o_maxPool2[1],2))
 
         self.denseLayer = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(in_features=400, out_features=120),
+            nn.Linear(in_features=cantidad_entradas, out_features=full_l1),
             nn.ReLU(),
-            nn.Linear(in_features=120, out_features=84),
+            nn.Linear(in_features=full_l1, out_features=full_l2),
             nn.ReLU(),
-            nn.Linear(in_features=84, out_features=10)
+            nn.Linear(in_features=full_l2, out_features=10)
         )
-    
+
     def forward(self, x):
         logits = self.layer1(x)
         logits = self.layer2(logits)
