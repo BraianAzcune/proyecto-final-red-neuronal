400fa595ceb30cf7e14e6c0da76770149c714f7f
BraianAzcune <braian.s.azcune@hotmail.com>
2022-11-15 01:09:16 -0300

    partial-fix|corriguiendo que todas las dimensiones sean validas

--- modelo_convolucional.py
+++ modelo_convolucional.py
@@ -1,56 +1,62 @@
-from numpy import pad
-import torch
 from torch import nn
 from math import floor
 
 
 def instanciarRed(config):
     modelo = RedConvolucional(
-        cant_filtros_conv1=config["cant_filtros_conv1"], 
+        cant_filtros_conv1=config["cant_filtros_conv1"],
         kernel_size_maxpool1=config["kernel_size_maxpool1"],
         cant_filtros_conv2=config["cant_filtros_conv2"],
         kernel_size_maxpool2=config["kernel_size_maxpool2"],
         full_l1=config["full_l1"],
         full_l2=config["full_l2"]
-        )
+    )
     return modelo
 
 
 class RedConvolucional(nn.Module):
+
+    def __calc_output_shape(self, input_shape, kernel_size, padding=0, stride=1):
+        # https://pytorch.org/docs/stable/generated/torch.nn.Conv2d.html
+        # nota en la formula "[]" significa funcion floor.
+        return floor((input_shape+2*padding-kernel_size)/stride)+1
+
     def __init__(self, cant_filtros_conv1=6, kernel_size_maxpool1=2, cant_filtros_conv2=16, kernel_size_maxpool2=2, full_l1=120, full_l2=84) -> None:
         super().__init__()
         # se reciben imagenes 3x32x32
         self.layer1 = nn.Sequential(
             # quedan un volumen de cant_filtros_conv1x30x30 # (n - k + s + p)/s
             nn.Conv2d(in_channels=3, out_channels=cant_filtros_conv1,
-                      kernel_size=5, stride=1, padding=2),
+                      kernel_size=5, stride=1, padding=1),
             nn.ReLU(),
             # queda un volumen de cant_filtros_conv1 x 15 x 15 (si kernel_size_maxpool1=2, si es 3, entonces= 14)
             nn.MaxPool2d(kernel_size=kernel_size_maxpool1, stride=2)
         )
+        self.o_conv2d = [cant_filtros_conv1, self.__calc_output_shape(32, 5, 1, 1)]
 
-        o_conv2d = [cant_filtros_conv1, 32-5+1+2]  # 30
-        o_maxPool = [cant_filtros_conv1, floor(
-            (o_conv2d[1] - kernel_size_maxpool1 + 2)/2)]
+        self.o_maxPool = [cant_filtros_conv1, self.__calc_output_shape(
+            self.o_conv2d[1], kernel_size_maxpool1, 0, 2)]
 
         self.layer2 = nn.Sequential(
-            nn.Conv2d(in_channels=o_maxPool[0], out_channels=cant_filtros_conv2,
+            nn.Conv2d(in_channels=self.o_maxPool[0], out_channels=cant_filtros_conv2,
                       kernel_size=5, stride=1, padding=0),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=kernel_size_maxpool2, stride=2)
         )
 
-        o_conv2d2 = [cant_filtros_conv2, o_maxPool[1] - 5 + 1]
-        o_maxPool2 = [cant_filtros_conv2, floor(
-            (o_conv2d2[1]-kernel_size_maxpool2+2)/2)]
+        self.o_conv2d2 = [cant_filtros_conv2,
+                     self.__calc_output_shape(self.o_maxPool[1], 5, 1, 1)]
 
-        cantidad_entradas = floor(o_maxPool2[0] * pow(o_maxPool2[1], 2))
+        
+        self.o_maxPool2 = [cant_filtros_conv2, self.__calc_output_shape(self.o_conv2d2[1],kernel_size_maxpool2,0,2)]
+
+        self.cantidad_entradas = self.o_maxPool2[0] * pow(self.o_maxPool2[1], 2)
 
         self.denseLayer = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(in_features=cantidad_entradas, out_features=full_l1),
+            nn.Linear(in_features=self.cantidad_entradas, out_features=full_l1),
             nn.ReLU(),
-            #nn.Dropout(0.1),
+            # nn.Dropout(0.1),
             nn.Linear(in_features=full_l1, out_features=full_l2),
             nn.ReLU(),
             nn.Linear(in_features=full_l2, out_features=10)
