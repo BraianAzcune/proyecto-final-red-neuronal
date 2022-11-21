8a9879bbba956d1dfb3057ec8ef3329339da4bec
BraianAzcune <braian.s.azcune@hotmail.com>
2022-11-20 22:02:11 -0300

    add|preparando para correr las pruebas buscando el mejor batch y lr

--- cargar_datos.py
+++ cargar_datos.py
@@ -15,10 +15,10 @@ def cargar_datasets(directorio="./data"):
         [transforms.ToTensor(),
          ])  # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+    trainset = torchvision.datasets.CIFAR10(root=directorio, train=True,
                                             download=True, transform=transform)
 
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+    testset = torchvision.datasets.CIFAR10(root=directorio, train=False,
                                        download=True, transform=transform)
     return trainset,testset
 
